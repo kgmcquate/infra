5a552995ae4c0a31dcf76de3cb829dc2d413345d
Kevin McQuate <kgmcquate@gmail.com>
2023-04-13 18:53:16 -0500

    deploying ecs cluster

--- main.tf
+++ main.tf
@@ -1,26 +1,9 @@
-terraform {
-    backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "kgmcquate"
 
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-            name = "infra"
-        }
-    }
-
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 4.0"
-        }
-    }
-}
 
 provider "aws" {
   region = "us-east-1"
-#   access_key = "my-access-key"  #Creds provided in environment variables
-#   secret_key = "my-secret-key"
+  access_key = var.AWS_ACCESS_KEY_ID  #Creds provided in environment variables
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 
