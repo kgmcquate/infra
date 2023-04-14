2b4bf926119120cf58e707af36ff57f62e53e1de
Kevin McQuate <kgmcquate@gmail.com>
2023-04-13 19:25:10 -0500

    fixed backend

--- backend.tf
+++ backend.tf
@@ -17,8 +17,22 @@ terraform {
     }
 }
 
+
+variable "AWS_REGION" {
+    type    = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+    type    = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    type    = string
+}
+
+
 provider "aws" {
-#   region = "us-east-1"
-#   access_key = var.AWS_ACCESS_KEY_ID  #Creds provided in environment variables
-#   secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID 
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
