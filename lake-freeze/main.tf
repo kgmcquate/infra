ce7d1abc591d792be475247965ac0c74a05696a1
Kevin McQuate <kgmcquate@gmail.com>
2023-04-14 16:22:47 -0500

    added rds db

--- lake-freeze/main.tf
+++ lake-freeze/main.tf
@@ -22,3 +22,44 @@ resource aws_ecs_cluster_capacity_providers capacity_provider {
 
     capacity_providers = ["FARGATE"]
 }
+
+variable postgres_pwd {
+  type = string
+}
+
+
+resource "aws_kms_key" "db_key" {
+  description = "KMS key for encrypting database"
+}
+
+resource "aws_rds_cluster" "db" {
+    cluster_identifier      = "lake-freeze"
+    engine                  = "aurora-postgresql"
+    engine_mode = "serverless"
+    port = 5432
+    availability_zones      = ["us-east-1d", "us-east-1f"]
+    database_name           = "default"
+    master_username         = "postgres"
+    master_password         = var.postgres_pwd
+    # kms_key_id = aws_kms_key.db_key.id
+    iam_database_authentication_enabled = true
+    iam_roles = []
+
+    backup_retention_period = 7
+    preferred_backup_window = "07:00-09:00"
+
+    enable_http_endpoint  = false
+
+    serverlessv2_scaling_configuration {
+      max_capacity = 0.5
+      min_capacity = 2.0
+    }
+}
+
+
+resource "aws_rds_cluster_instance" "example" {
+  cluster_identifier = aws_rds_cluster.db.id
+  instance_class     = "db.serverless"
+  engine             = aws_rds_cluster.db.engine
+  engine_version     = aws_rds_cluster.db.engine_version
+}
