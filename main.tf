272d82ee937d860fbfc89828f405d91121e209c9
EC2 Default User <ec2-user@ip-172-31-34-68.ec2.internal>
2023-04-16 23:07:49 +0000

    fixing s3 bucket

--- main.tf
+++ main.tf
@@ -10,6 +10,16 @@ module "lake-freeze" {
 
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "deployment-zone" {
+resource "aws_s3_bucket" "deployment_zone" {
   bucket = "deployment-zone-${data.aws_caller_identity.current.account_id}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "deployment-zone-block-public" {
+  bucket = aws_s3_bucket.deployment_zone.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
