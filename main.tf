00f416fc6a3d0e1a75c0ca9bada6493f0e3ae387
EC2 Default User <ec2-user@ip-172-31-34-68.ec2.internal>
2023-04-16 21:39:31 +0000

    added deployment bucket

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ module "lake-freeze" {
     source = "./lake-freeze"
     POSTGRES_PWD = var.POSTGRES_PWD
 }
+
+data "aws_caller_identity" "current" {}
+
+locals {
+    account_id = data.aws_caller_identity.current.account_id
+    region = data.aws_caller_identity.current.region
+}
+
+
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "deployment-zone-${local.account_id}-${local.region}"
+}
