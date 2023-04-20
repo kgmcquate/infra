0c9d9cc0f037b84f88e52bd1ad71e177b2cd3c8e
kgmcquate <59513919+kgmcquate@users.noreply.github.com>
2023-04-19 21:18:12 -0500

    Update main.tf

--- lake-freeze/main.tf
+++ lake-freeze/main.tf
@@ -152,26 +152,14 @@ resource "aws_security_group" "lambda_sg" {
   description = "Security Group for Lambda Egress"
   vpc_id      = aws_default_vpc.default.id
 
-  ingress {
+  egress {
     description      = "inbound rules"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 5432
+    to_port          = 5432
     protocol         = "tcp"
     cidr_blocks      = [aws_default_vpc.default.cidr_block]
 #     ipv6_cidr_blocks = [aws_default_vpc.default.ipv6_cidr_block]
   }
-
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
-
-#   tags = {
-#     Name = "allow_tls"
-#   }
 }
 
 
@@ -180,12 +168,13 @@ resource "aws_security_group" "rds_sg" {
   description = "Security Group for RDS"
   vpc_id      = aws_default_vpc.default.id
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  ingress {
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = [aws_default_vpc.default.cidr_block]
+    security_groups = aws_security_group.lambda_sg.id
+    
   }
 
 #   tags = {
