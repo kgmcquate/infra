3152e6fa77888c0d920402ff02c8221c1ddef1fc
kgmcquate <59513919+kgmcquate@users.noreply.github.com>
2023-04-20 22:25:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,49 @@ resource "aws_s3_bucket_public_access_block" "deployment-zone-block-public" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+  
+data "aws_region" "current" {}
+  
+data "aws_security_group" "default" {
+  id = "sg-e0fa90d6"
+}
+  
+resource "aws_default_subnet" "a" {
+  availability_zone = aws_region.current.name + "a"
+}
+  
+resource "aws_default_subnet" "b" {
+  availability_zone = aws_region.current.name + "b"
+}
+
+resource "aws_default_subnet" "c" {
+  availability_zone = aws_region.current.name + "c"
+}
+  
+  
+resource "aws_vpc_endpoint" "secretsmanager" {
+  vpc_id = aws_default_vp.default.id
+  service_name = "com.amazonaws.us-east-1.secretsmanager"
+  policy = "Interface"
+  subnet_ids = [
+    aws_default_subnet.a.id,
+    aws_default_subnet.b.id,
+    aws_default_subnet.c.id,
+  ]
+  
+  security_group_ids = [aws_security_group.default.id]
+  
+  ip_address_type = "ipv4"
+  
+  private_dns_enabled = true
+  
+}
+
+  
