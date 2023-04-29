067cf3ee530c97dcf155d500e9bde1144852cbca
Kevin McQuate <kgmcquate@gmail.com>
2023-04-29 13:16:22 -0500

    removed sm endpoint

--- main.tf
+++ main.tf
@@ -50,22 +50,22 @@ resource "aws_default_subnet" "c" {
 }
   
   
-resource "aws_vpc_endpoint" "secretsmanager" {
-  vpc_id = aws_default_vpc.default.id
-  service_name = "com.amazonaws.us-east-1.secretsmanager"
-  vpc_endpoint_type = "Interface"
-  subnet_ids = [
-    aws_default_subnet.a.id,
-    aws_default_subnet.b.id,
-    aws_default_subnet.c.id,
-  ]
+# resource "aws_vpc_endpoint" "secretsmanager" {
+#   vpc_id = aws_default_vpc.default.id
+#   service_name = "com.amazonaws.us-east-1.secretsmanager"
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids = [
+#     aws_default_subnet.a.id,
+#     aws_default_subnet.b.id,
+#     aws_default_subnet.c.id,
+#   ]
   
-  security_group_ids = [data.aws_security_group.default.id]
+#   security_group_ids = [data.aws_security_group.default.id]
   
-  ip_address_type = "ipv4"
+#   ip_address_type = "ipv4"
   
-  private_dns_enabled = true
+#   private_dns_enabled = true
   
-}
+# }
 
   
