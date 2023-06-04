7aa9baf0aa67d89b79633c2c4b120d359959f856
Kevin McQuate <kgmcquate@gmail.com>
2023-06-04 10:52:04 -0500

    added emr studio for emr serverless

--- lake-freeze/emr.tf
+++ lake-freeze/emr.tf
@@ -5,10 +5,10 @@
 resource "aws_emr_studio" "default_studio" {
   auth_mode                   = "IAM"
   default_s3_location         = "s3://${aws_s3_bucket.emr_zone.bucket}/studios/default-studio/"
-  engine_security_group_id    = aws_security_group.test.id
+  engine_security_group_id    = data.aws_security_group.default.id
   name                        = "default-studio"
   service_role                = aws_iam_role.backend_role.arn  
   vpc_id                      = aws_default_vpc.default.id
   subnet_ids                  = [aws_default_subnet.a.id]
-  workspace_security_group_id = aws_security_group.default.id
+  workspace_security_group_id = data.aws_security_group.default.id
 }
