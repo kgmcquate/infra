94c924d87c694596f8a56338d5a6e71df392cbe2
Kevin McQuate <kgmcquate@gmail.com>
2023-07-02 11:50:04 -0500

    migrating to new vpc

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "lake-freeze" {
     source = "./lake-freeze"
     POSTGRES_PWD = var.POSTGRES_PWD
     vpc_id = module.vpc.vpc_id
-    subnet_id = module.vpc.private_subnets[0]
+    subnet_ids = module.vpc.private_subnets
 
     depends_on = [ module.vpc ]
 }
