029eb2cd9285dffa11c735fffc37f6133f10fa40
Kevin McQuate <kgmcquate@gmail.com>
2023-07-03 00:22:04 -0500

    migrating to new vpc

--- ec2.tf
+++ ec2.tf
@@ -86,6 +86,28 @@ resource "aws_security_group_rule" "ssh" {
   protocol          = -1
 }
 
+
+
+resource "aws_security_group_rule" "nat_ssh" {
+  security_group_id = module.nat.sg_id
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 65535
+  protocol          = -1
+}
+
+resource "aws_security_group_rule" "ssh" {
+  security_group_id = module.vpc.default_security_group_id
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 65535
+  protocol          = -1
+}
+
+
+
 # resource "aws_security_group_rule" "ssh" {
 #   security_group_id = module.vpc.default_security_group_id
 #   type              = "ingress"
