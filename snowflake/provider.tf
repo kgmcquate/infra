14ecb2643027cc318461759a2449d93cb38d32de
kevin <you@example.com>
2024-01-02 16:12:20 -0600

    added snowflake

--- snowflake/provider.tf
+++ snowflake/provider.tf
@@ -11,5 +11,4 @@ provider "snowflake" {
   account                = var.provider_account # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT env var
   user                   = var.provider_username # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
   password               = var.provider_password
-  role      = "ACCOUNTADMIN"
 }
