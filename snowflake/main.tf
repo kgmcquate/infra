5a4b978620ae7cd85d04264f46cbf948cda6464f
kevin <you@example.com>
2024-01-02 17:31:18 -0600

    capitalize snowflake object names

--- snowflake/main.tf
+++ snowflake/main.tf
@@ -4,18 +4,13 @@
 #   }
 # }
 
-# resource "snowflake_user" "dbt_testgen" {
-#   name         = "DBT_TESTGEN"
-#   password     = var.dbt_testgen_password
+resource "snowflake_user" "dbt_testgen" {
+  name         = "DBT_TESTGEN"
+  password     = var.dbt_testgen_password
 
-# #   default_warehouse       = snowflake_warehouse.dbt_testgen.name
-# #   default_role            = snowflake_role.dbt_testgen.name
-#   lifecycle {
-#     replace_triggered_by = [
-#       null_resource.always_run
-#     ]
-#   }
-# }
+#   default_warehouse       = snowflake_warehouse.dbt_testgen.name
+#   default_role            = snowflake_role.dbt_testgen.name
+}
 
 # resource "snowflake_role" "dbt_testgen" {
 #   name    = "DBT_TESTGEN"
