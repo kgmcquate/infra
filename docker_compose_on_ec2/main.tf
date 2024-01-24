625a98376fa8b2eab62eaa94eb750f9c67c27294
kevin <you@example.com>
2024-01-24 17:17:32 -0600

    fixed databricks provider

--- docker_compose_on_ec2/main.tf
+++ docker_compose_on_ec2/main.tf
@@ -53,13 +53,14 @@ systemctl start my_custom_service
 EOF
 }
 
-data "aws_ami" "latest_amazon_linux" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn*"]
-    }
+data "aws_ami" "al_arm64" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["al2023-ami-*-kernel-*-arm64"]
+  }
+  owners = ["amazon"]
 }
 
 resource "aws_ebs_volume" "persistent" {
