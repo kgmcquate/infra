57b86a4c84fe8c7511e3aa55908eba0dba29bc4c
kevin <you@example.com>
2024-01-25 07:47:10 -0600

    removing users

---
+++ databricks/users.tf
@@ -0,0 +1,28 @@
+data "databricks_user" "me" {
+  provider = databricks
+  user_name = "kgmcquate@gmail.com"
+}
+
+# resource "databricks_user" "me2" {
+#   user_name = "fuzzh3d@gmail.com"
+# }
+
+# resource "databricks_entitlements" "me" {
+#   provider = databricks_ws
+#   user_id                    = data.databricks_user.me.id
+#   allow_cluster_create       = true
+#   allow_instance_pool_create = true
+#   workspace_access = true
+# }
+
+resource "databricks_mws_permission_assignment" "add_user" {
+  provider = databricks_ws
+  workspace_id = databricks_mws_workspaces.this.workspace_id
+  principal_id = data.databricks_user.me.id
+  permissions  = ["USER"] # ["ADMIN"]#
+}
+
+# resource "databricks_permission_assignment" "add_user" {
+#   principal_id = data.databricks_user.me.id
+#   permissions  = ["USER"]
+# }
