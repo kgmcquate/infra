8f44387a56029e6dc1edf5b661b594fc1dfd22a9
kevin <you@example.com>
2024-01-24 23:18:14 -0600

    databricks

--- databricks/workspace/users.tf
+++ databricks/workspace/users.tf
@@ -2,12 +2,12 @@ data "databricks_user" "me" {
   user_name = "kgmcquate@gmail.com"
 }
 
-resource "databricks_entitlements" "me" {
-  user_id                    = data.databricks_user.me.id
-  allow_cluster_create       = true
-  allow_instance_pool_create = true
-  workspace_access = true
-}
+# resource "databricks_entitlements" "me" {
+#   user_id                    = data.databricks_user.me.id
+#   allow_cluster_create       = true
+#   allow_instance_pool_create = true
+#   workspace_access = true
+# }
 
 # resource "databricks_mws_permission_assignment" "add_user" {
 #   workspace_id = databricks_mws_workspaces.this.workspace_id
