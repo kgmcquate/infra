403b220ed68ce47d92c9dec16574409aebe1aa3a
kevin <you@example.com>
2024-01-24 22:19:14 -0600

    databricks

--- databricks/aws/vpc.tf
+++ databricks/aws/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   version = "5.5.1"
 
   name = var.name
-  cidr = var.cidr_block
+  cidr = "10.1.0.0/16"
   tags = var.tags
 
   enable_dns_hostnames = true
@@ -13,8 +13,8 @@ module "vpc" {
   # create_igw           = true
 
   azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets      = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets       = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
 
   manage_default_security_group = true
   default_security_group_name   = "${var.name}-sg"
