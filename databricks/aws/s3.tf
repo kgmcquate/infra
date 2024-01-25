d3a06fb7bafb372bf30523f2fa411b355637cb3c
kevin <you@example.com>
2024-01-25 17:13:10 -0600

    adding pulsar confs

--- databricks/aws/s3.tf
+++ databricks/aws/s3.tf
@@ -6,7 +6,7 @@ data aws_caller_identity current {}
 data aws_region current {}
 
 resource "aws_s3_bucket" "root_storage_bucket" {
-  bucket        = "${var.name}-rootbucket"
+  bucket        = "${var.name}-databricks-root-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
   force_destroy = true
   tags = merge(var.tags, {
     Name = "${var.name}-databricks-root-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
