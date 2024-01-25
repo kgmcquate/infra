34ffebae452e669410942571d0832edcab802dee
kevin <you@example.com>
2024-01-25 17:59:16 -0600

    adding pulsar confs

--- databricks/users.tf
+++ databricks/users.tf
@@ -3,13 +3,13 @@ data "databricks_user" "me" {
   user_name = "kgmcquate@gmail.com"
 }
 
-# resource "databricks_user" "me2" {
-#   provider = databricks.main-ws
-#   user_name = "fuzzh3d@gmail.com"
-#   allow_cluster_create       = true
-#   allow_instance_pool_create = true
-#   workspace_access = true
-# }
+resource "databricks_user" "me2" {
+  provider = databricks.main-ws
+  user_name = "fuzzh3d@gmail.com"
+  allow_cluster_create       = true
+  allow_instance_pool_create = true
+  workspace_access = true
+}
 
 # resource "databricks_entitlements" "me" {
 #   provider = databricks
