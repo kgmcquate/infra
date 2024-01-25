85b7b598d9c5d94b5a5cb6ae3460911672836841
kevin <you@example.com>
2024-01-24 23:10:14 -0600

    databricks

--- databricks/workspace/users.tf
+++ databricks/workspace/users.tf
@@ -8,3 +8,9 @@ resource "databricks_entitlements" "me" {
   allow_instance_pool_create = true
   workspace_access = true
 }
+
+resource "databricks_mws_permission_assignment" "add_user" {
+  workspace_id = databricks_mws_workspaces.this.workspace_id
+  principal_id = data.databricks_user.me.id
+  permissions  = ["ADMIN"]#["USER"]
+}
