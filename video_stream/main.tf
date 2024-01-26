8074c93fd602f81d563710b57c20c516c88744e1
kevin <you@example.com>
2024-01-26 16:59:01 -0600

    added port to provider

--- video_stream/main.tf
+++ video_stream/main.tf
@@ -9,8 +9,19 @@ variable jwt_token {}
 locals {
     domain = "kevin-mcquate.net"
     pulsar_superuser_secret_name = "video_stream_pulsar_superuser_token"
+    broker_port = 80
+    cluster_name = "cluster-a"
+    superuser_name = "superuser"
 }
 
+data "template_file" "docker-compose" {
+  template = "${file("${path.module}/docker-compose.template.yml")}"
+  vars = {
+    broker_port = "${local.broker_port}"
+    cluster_name = local.cluster_name
+    superuser_name = local.superuser_name
+  }
+}
 
 module "video_stream_pulsar" {
     source =  "../docker_compose_on_ec2"
@@ -18,7 +29,7 @@ module "video_stream_pulsar" {
     key_name = var.ssh_keypair
     instance_type = "t4g.small"
     iam_instance_profile = aws_iam_instance_profile.pulsar_profile.name
-    docker_compose_str = file("${path.module}/docker-compose.yml")
+    docker_compose_str = data.template_file.docker-compose.rendered
     before_docker_compose_script = "mkdir -p /root/key/ && echo \"${var.jwt_secret_key_base64}\" | base64 -d > /root/key/secret.key "
     subnet_id = var.subnet_ids[2]
     availability_zone = var.availability_zones[2]
--- video_stream/pulsar.tf
+++ video_stream/pulsar.tf
@@ -1,7 +1,7 @@
 resource pulsar_tenant tenant {
     tenant = "video_stream"
-    admin_roles = ["superuser"]
-    allowed_clusters = ["cluster-a"]
+    admin_roles = [local.superuser_name]
+    allowed_clusters = [local.cluster_name]
 }
 
 resource pulsar_namespace namespace {
@@ -15,11 +15,11 @@ resource pulsar_namespace namespace {
     
 }
 
-resource pulsar_topic raw_frames {
-    tenant = "video_stream"
-    namespace = "video_stream"
-    topic_name = "raw-livestream-frames"
-    topic_type = "persistent"
-    partitions = 4
-    depends_on = [ pulsar_tenant.tenant, pulsar_namespace.namespace ]
-}
+# resource pulsar_topic raw_frames {
+#     tenant = "video_stream"
+#     namespace = "video_stream"
+#     topic_name = "raw-livestream-frames"
+#     topic_type = "persistent"
+#     partitions = 4
+#     depends_on = [ pulsar_tenant.tenant, pulsar_namespace.namespace ]
+# }
