8632a4d34a6f157764edc297794bc3287a874dd8
kevin <you@example.com>
2024-01-26 22:12:29 -0600

    added port to provider

--- video_stream/pulsar/pulsar.tf
+++ video_stream/pulsar/pulsar.tf
@@ -4,24 +4,24 @@ variable superuser_name {}
 variable cluster_name {}
 variable jwt_token {}
 
-resource pulsar_tenant tenant {
-    tenant = "video_stream"
-    admin_roles = [var.superuser_name]
-    allowed_clusters = [var.cluster_name]
+# resource pulsar_tenant tenant {
+#     tenant = "video_stream"
+#     admin_roles = [var.superuser_name]
+#     allowed_clusters = [var.cluster_name]
 
-    depends_on = [ var.broker_host ]
-}
+#     depends_on = [ var.broker_host ]
+# }
 
-resource pulsar_namespace namespace {
-    tenant = "video_stream"
-    namespace = "video_stream"
-    # permission_grant = {
-    #     actions = ["create"]
-    #     role = "superuser"
-    # }
-    depends_on = [ pulsar_tenant.tenant, var.broker_host ]
+# resource pulsar_namespace namespace {
+#     tenant = "video_stream"
+#     namespace = "video_stream"
+#     # permission_grant = {
+#     #     actions = ["create"]
+#     #     role = "superuser"
+#     # }
+#     depends_on = [ pulsar_tenant.tenant, var.broker_host ]
     
-}
+# }
 
 # resource pulsar_topic raw_frames {
 #     tenant = "video_stream"
