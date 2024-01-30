446d95054571a09470d594166cc65dbb3d1b78d8
kevin <you@example.com>
2024-01-30 17:55:55 -0600

    changed name of databricks

--- databricks/main.tf
+++ databricks/main.tf
@@ -6,27 +6,27 @@ module aws {
   databricks_account_id = var.databricks_account_id
 }
 
-# module workspace {
-#   source                = "./workspace"
-#   name = var.name
-#   region = var.region
-#   databricks_account_id = var.databricks_account_id
-#   cross_account_role_arn = module.aws.cross_account_role_arn
-#   root_storage_bucket = module.aws.root_storage_bucket
-#   security_group_id = module.aws.default_security_group_id
-#   subnet_ids = module.aws.private_subnets
-#   vpc_id = module.aws.vpc_id
-# }
+module workspace {
+  source                = "./workspace"
+  name = var.name
+  region = var.region
+  databricks_account_id = var.databricks_account_id
+  cross_account_role_arn = module.aws.cross_account_role_arn
+  root_storage_bucket = module.aws.root_storage_bucket
+  security_group_id = module.aws.default_security_group_id
+  subnet_ids = module.aws.private_subnets
+  vpc_id = module.aws.vpc_id
+}
 
 
 
-# module main_workspace {
-#   source                = "./main_workspace"
+module main_workspace {
+  source                = "./main_workspace"
 
-#   # admin_user = data.databricks_user.me
+  # admin_user = data.databricks_user.me
 
-#   providers = {
-#     databricks.main-ws = databricks.main-ws
-#   }
-#   depends_on = [module.workspace]
-# }
+  providers = {
+    databricks.main-ws = databricks.main-ws
+  }
+  depends_on = [module.workspace]
+}
