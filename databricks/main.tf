5325cdc6504fb82bc87e08a5b018d7758bd99d70
kevin <you@example.com>
2024-01-30 16:08:27 -0600

    changed name of databricks

--- databricks/main.tf
+++ databricks/main.tf
@@ -6,17 +6,17 @@ module aws {
   databricks_account_id = var.databricks_account_id
 }
 
-# module workspace {
-#   source                = "./workspace"
-#   name = var.name
-#   region = var.region
-#   databricks_account_id = var.databricks_account_id
-#   cross_account_role_arn = module.aws.cross_account_role_arn
-#   root_storage_bucket = module.aws.root_storage_bucket
-#   security_group_id = module.aws.default_security_group_id
-#   subnet_ids = module.aws.public_subnets
-#   vpc_id = module.aws.vpc_id
-# }
+module workspace {
+  source                = "./workspace"
+  name = var.name
+  region = var.region
+  databricks_account_id = var.databricks_account_id
+  cross_account_role_arn = module.aws.cross_account_role_arn
+  root_storage_bucket = module.aws.root_storage_bucket
+  security_group_id = module.aws.default_security_group_id
+  subnet_ids = module.aws.public_subnets
+  vpc_id = module.aws.vpc_id
+}
 
 
 
