3a9bccd45daf3df63fe70f3fec0e45fa2568aec3
kevin <you@example.com>
2024-01-30 19:07:03 -0600

    changed name of databricks

--- databricks/main_workspace_objects/clusters.tf
+++ databricks/main_workspace_objects/clusters.tf
@@ -1,5 +1,5 @@
 
-# resource "databricks_instance_profile" "main_profile" {
-#   provider = databricks.main-ws
-#   instance_profile_arn = var.instance_profile_arn
-# }
+resource "databricks_instance_profile" "main_profile" {
+  provider = databricks.main-ws
+  instance_profile_arn = var.instance_profile_arn
+}
