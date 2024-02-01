c1773ee0217b2a0bde17dd72cb1202fd1c3cfb2c
kevin <you@example.com>
2024-01-31 20:59:08 -0600

    added airflow config

--- airflow/main.tf
+++ airflow/main.tf
@@ -3,14 +3,23 @@ variable security_group_ids {}
 variable availability_zone {}
 variable ssh_keypair {}
 variable domain {}
-
+variable airflow_s3_bucket {}
+variable airflow_s3_prefix {
+    default = "airflow/"
+}
 
 module "airflow" {
     source =  "../docker_compose_on_ec2"
     name = "airflow"
     key_name = var.ssh_keypair
     instance_type = "t4g.small"
-    before_docker_compose_script = "docker-compose up airflow-init"
+    before_docker_compose_script = <<-EOF
+mkdir -p /opt/airflow/
+{ crontab -l; echo "30 23 * * * aws s3 sync /opt/airflow/ s3://${var.airflow_s3_bucket}/${var.airflow_s3_prefix}"; } | crontab
+
+export _AIRFLOW_WWW_USER_PASSWORD='${random_password.password.result}'
+docker-compose up airflow-init
+EOF
     docker_compose_str = file("${path.module}/docker-compose.yml")
     subnet_id = var.subnet_id
     availability_zone = var.availability_zone
@@ -19,18 +28,4 @@ module "airflow" {
     persistent_volume_size_gb = 1
 }
 
-data "aws_route53_zone" "primary" {
-  name = var.domain
-}
 
-locals {
-    airflow_domain = "airflow.${var.domain}"
-}
-
-resource "aws_route53_record" "www" {
-  zone_id = data.aws_route53_zone.primary.zone_id
-  name    = local.airflow_domain
-  type    = "A"
-  ttl     = 300
-  records = [module.airflow.public_ip]
-}
