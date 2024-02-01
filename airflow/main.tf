77076ac5754be91a07a5cb75dc8ce7530b0b6c21
kevin <you@example.com>
2024-01-31 21:28:14 -0600

    added airflow config

--- airflow/main.tf
+++ airflow/main.tf
@@ -8,18 +8,28 @@ variable airflow_s3_prefix {
     default = "airflow/"
 }
 
+locals {
+    startup_script = <<-EOF
+#!/bin/bash
+set -Eeuxo pipefail
+
+mkdir -p /opt/airflow/
+
+systemd-run --on-boot=1 --on-unit-active=300 aws s3 sync s3://${var.airflow_s3_bucket}/${var.airflow_s3_prefix} /opt/airflow/
+
+export AIRFLOW_PROJ_DIR=/opt/airflow/
+export _AIRFLOW_WWW_USER_PASSWORD='${random_password.password.result}'
+docker-compose up airflow-init
+
+EOF
+}
+
 module "airflow" {
     source =  "../docker_compose_on_ec2"
     name = "airflow"
     key_name = var.ssh_keypair
     instance_type = "t4g.small"
-    before_docker_compose_script = <<-EOF
-mkdir -p /opt/airflow/
-{ crontab -l; echo "30 23 * * * aws s3 sync /opt/airflow/ s3://${var.airflow_s3_bucket}/${var.airflow_s3_prefix}"; } | crontab
-
-export _AIRFLOW_WWW_USER_PASSWORD='${random_password.password.result}'
-docker-compose up airflow-init
-EOF
+    before_docker_compose_script = local.startup_script
     docker_compose_str = file("${path.module}/docker-compose.yml")
     subnet_id = var.subnet_id
     availability_zone = var.availability_zone
