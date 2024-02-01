9b9fb6dfcf0e5195d3aaffc9685b00c23281ea5f
kevin <you@example.com>
2024-01-31 20:59:40 -0600

    added airflow config

--- main.tf
+++ main.tf
@@ -60,6 +60,7 @@ module airflow {
   availability_zone = module.vpc.azs[2]
   security_group_ids = [aws_security_group.allow_all.id]
   ssh_keypair = aws_key_pair.ssh.key_name
+  airflow_s3_bucket = aws_s3_bucket.deployment_zone.bucket
 
   domain = var.main_domain
 }
