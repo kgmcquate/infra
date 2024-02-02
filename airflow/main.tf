de5e09124426fe18c067d0d7a4b64413144e97c3
kevin <you@example.com>
2024-02-01 18:18:40 -0600

    added airflow config

--- airflow/main.tf
+++ airflow/main.tf
@@ -33,7 +33,7 @@ module "airflow" {
     key_name = var.ssh_keypair
     instance_type = "t4g.medium"
     before_docker_compose_script = local.startup_script
-    docker_compose_str = replace(file("${path.module}/docker-compose.template.yml"), "airflow_admin_password", random_password.password.result)
+    docker_compose_str = file("${path.module}/docker-compose.template.yml") #replace(, "airflow_admin_password", random_password.password.result)
     subnet_id = var.subnet_id
     availability_zone = var.availability_zone
     vpc_security_group_ids = var.security_group_ids
