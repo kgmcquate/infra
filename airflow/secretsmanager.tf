54778d1dce605590b686be76a3b87db1efee0c84
kevin <you@example.com>
2024-02-01 21:00:44 -0600

    added password

--- airflow/secretsmanager.tf
+++ airflow/secretsmanager.tf
@@ -1,4 +1,4 @@
-resource "random_password" "password" {
+resource "random_password" "airflow_admin_password" {
   length           = 20
   special          = true
 }
@@ -10,5 +10,5 @@ resource "aws_secretsmanager_secret" "airflow_admin_password" {
 # Store private key in secretsmanager for later access
 resource "aws_secretsmanager_secret_version" "private_key_version" {
   secret_id = aws_secretsmanager_secret.airflow_admin_password.id
-  secret_string = random_password.password.result
+  secret_string = random_password.airflow_admin_password.result
 }
