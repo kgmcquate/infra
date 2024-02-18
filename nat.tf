5a5e459a59580d13b9cec2a1507aa1d84913ee1b
kevin <you@example.com>
2024-02-18 10:07:50 -0600

    renaming nat

--- nat.tf
+++ nat.tf
@@ -1,7 +1,7 @@
 module "nat" {
   source = "int128/nat-instance/aws"
 
-  name                        = "nat"
+  name                        = "main-nat"
   key_name                    = aws_key_pair.ssh.key_name
   vpc_id                      = module.vpc.vpc_id
   public_subnet               = module.vpc.public_subnets[0]
