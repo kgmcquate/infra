2510fbd9a7f984d3bf070177b5580d8a3f29d52b
kevin <you@example.com>
2024-02-18 10:18:31 -0600

    renaming nat

--- airflow/main.tf
+++ airflow/main.tf
@@ -24,6 +24,7 @@ locals {
     startup_script = <<-EOF
 cat > Dockerfile <<-"FILE"
 FROM apache/airflow:2.8.1-python3.11
+
 USER root
 RUN mkdir -p /opt/airflow/logs/ &&  \
     mkdir -p /opt/airflow/logs/scheduler &&  \
