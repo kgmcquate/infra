0ba7d9737986ec1c0f76de56155fce2dffd9f891
kevinmcquate <kgmcquate@gmail.com>
2025-02-01 09:04:11 -0500

    added vultr k8s cluster

--- backend.tf
+++ backend.tf
@@ -1,14 +1,4 @@
 terraform {
-    # backend "remote" {
-    #     # The name of your Terraform Cloud organization.
-    #     organization = "kgmcquate"
-
-    #     # The name of the Terraform Cloud workspace to store Terraform state files in.
-    #     workspaces {
-    #         name = "infra"
-    #     }
-    # }
-
     backend "s3" {}
 
     required_providers {
@@ -22,5 +12,3 @@ terraform {
         }
     }
 }
-
-
