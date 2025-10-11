cf13d6498834ad1780ed32d8c55857c1efc8f4ce
kevinmcquate <kgmcquate@gmail.com>
2025-10-10 22:17:45 -0400

    adding secret

--- azure/main.tf
+++ azure/main.tf
@@ -77,3 +77,15 @@ resource "azurerm_key_vault" "main" {
 #     ]
 #   }
 }
+
+
+resource "azurerm_key_vault_secret" "dagster_oauth_client_id" {
+  name         = "dagster-oauth-client-id"
+  value        = jsonencode({
+    client_id     = azuread_application.auth.application_id,
+    tenant_id     = data.azurerm_client_config.current.tenant_id,
+    subscription_id = data.azurerm_client_config.current.subscription_id,
+    client_secret = azuread_service_principal_password.pass.value
+  })
+  key_vault_id = azurerm_key_vault.main.id
+}
